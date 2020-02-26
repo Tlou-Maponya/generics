40f27b565db8a6a9ba254e4f2d1a0c930c82f5c0
Tlou <tlou.maponya@umuzi.org>
2020-02-26 10:54:50 +0200

    generics class commit

---
+++ src/main/java/GenericsClass.java
@@ -0,0 +1,12 @@
+public class GenericsClass<T> {
+    public T type;
+
+    public void setType(T type) {
+        this.type = type;
+    }
+    public T getType() {
+        return type;
+    }
+
+
+}
--- src/main/java/MainProgram.java
+++ src/main/java/MainProgram.java
@@ -3,8 +3,19 @@ public class MainProgram {
         Integer [] numbers = {1 ,9 ,9 ,3 ,1 ,2 ,0 ,9};
         String [] text = {"A", "Legend", "was", "born!"};
 
+        // Method to output array content of either type int or type string
         GenericsMethod.printArrayContent(numbers);
         GenericsMethod.printArrayContent(text);
+
+        // Class that sets and gets a private single variable which can either be String or Integer
+        GenericsClass<String> name = new GenericsClass<>();
+        name.setType("Tlou Maponya");
+        GenericsClass<Integer> age = new GenericsClass<>();
+        age.setType(27);
+
+        System.out.println(name.getType());
+        System.out.println(age.getType());
+
 
     }
 }
